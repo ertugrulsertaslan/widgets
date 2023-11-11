bc6fd52c43a0b6724cf049b2a8cc08810767d9cf
Ertuğrul Sertaslan <ertugrulsertaslan@gmail.com>
2023-11-11 18:33:54 +0300

    add eyes widget properties

---
+++ eyes.js
@@ -0,0 +1,14 @@
+let pupils = document.getElementsByClassName("pupil");
+
+
+addEventListener("mousemove", (event) => {
+    let x = event.clientX*100/window.innerWidth + "%";
+    let y = event.clientY*100/window.innerHeight + "%";
+    //console.log (x +"--"+y);
+    for (let i=0 ; i<2 ;i++){
+        pupils[i].style.top = y;
+        pupils[i].style.left = x;
+        pupils[i].style.transform="translate(-" + x +",-"+ y +")";
+    }
+});
+
