0dcf5fbfb3bd11e7bba071eb2a7ac1606d8e8430
Ertuğrul Sertaslan <ertugrulsertaslan@gmail.com>
2023-11-20 23:40:43 +0300

    fix eyes coordination

--- eyes.js
+++ eyes.js
@@ -2,7 +2,7 @@ let pupils = document.getElementsByClassName("pupil");
 
 
 addEventListener("mousemove", (event) => {
-    let x = event.clientX*100/window.innerWidth + "%";
+    let x = event.clientX*135/window.innerWidth + "%";
     let y = event.clientY*100/window.innerHeight + "%";
     //console.log (x +"--"+y);
     for (let i=0 ; i<2 ;i++){
