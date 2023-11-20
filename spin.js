3a87a568b9ffcec9d70ac19c09f7a0f899f74066
Ertuğrul Sertaslan <ertugrulsertaslan@gmail.com>
2023-11-20 23:43:34 +0300

    add spin font and fix spin style, properties

--- spin.js
+++ spin.js
@@ -6,18 +6,39 @@ let spinBtn = document.querySelector("button");
 
 spinBtn.addEventListener("click",()=>{
     spinBtn.disabled = true;
+    let spinListItem ;
+    let slot = [];  
+    for(let i=0; i < spinContent.length ;i++){
+        spinContent[i].innerHTML="";
+        spinBtn.innerText = "Spin";
+    }
     for(let i=0 ;i<3;i++){
+
         let spinList = document.createElement('ul');
         spinList.classList.add('spin-list');
+            
         spinContent[i].append(spinList);
-        
+        spinList.style["-webkit-animation-duration"] = i+2 + "s";
+
         for(let i=0;i<40;i++){
-            let spinListItem = document.createElement('li');
+            spinListItem = document.createElement('li');
             spinListItem.classList.add('spin-list-item');
             spinList.append(spinListItem);
-            spinListItem.innerText =  fruits[Math.floor(Math.random() * fruits.length)];
+            spinListItem.innerText =  fruits[Math.floor(Math.random() * fruits.length)];           
         }
-
+        slot[i] = spinList.children[1].innerText; 
         
     }
+    setTimeout(() => {
+        if (slot[0] == slot[1] ) {
+            if (slot[0] == slot[2]){
+                if (slot[1] == slot[2]){  
+                    message.innerText = "Jackpot" ;    
+                }    
+            } 
+                }else{
+                    spinBtn.innerText = "Try Again";
+                }
+                    spinBtn.disabled = false;
+    }, 4000);
  });
