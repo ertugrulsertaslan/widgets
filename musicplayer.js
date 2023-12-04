f6b295f15e037b0db2d30858e39f78eba55de3ac
Ertuğrul Sertaslan <ertugrulsertaslan@gmail.com>
2023-12-04 22:51:51 +0300

    add more music to playlist

--- musicplayer.js
+++ musicplayer.js
@@ -1,26 +1,53 @@
 let progress = document.getElementById("progress");
 let song = document.getElementById("song");
+
 let controlIcon = document.getElementById("stop-play-btn");
 let forwardIcon = document.getElementById("forward-btn");
 let backIcon = document.getElementById("back-btn");
+songs = ["sounds/Imagine Dragons - Birds.m4a",
+"sounds/The Drums - Days.m4a",
+"sounds/River Whyless Michigan Cherry.m4a"];
+
+let songPhoto = document.getElementById("song-photo-content");
+let songName = document.getElementById("song-name");
+let songArtist = document.getElementById("song-artist");
+let PlayStopButton = document.getElementById("img-btn");
+
+
+songsPhotos = ["photo/Imagine-Dragons-album-photo.jpg",
+              "photo/TheDrums-days.jpg",
+              "photo/MichiganCherry.jpg"];
+
+songsNames = ["Birds","Days","Michigan Cherry"];
+artistNames = ["Imagine Dragons","The Drums","River Whyless"];
+
+
 
 let play = 0;
+let songCount = 0; 
 
 song.onloadedmetadata = function(){
     progress.max = song.duration ;
     progress.value = song.currentTime ;
 }
 
+if(songCount == 0 ){
+        song.setAttribute('src', `${songs[songCount]}`);
+        song.setAttribute('type', 'video/mp4');
+        songName.innerText = songsNames[songCount];
+        songArtist.innerText = artistNames[songCount];
+        songPhoto.setAttribute('src', `${songsPhotos[songCount]}`);
+}
+
 controlIcon.addEventListener("click",()=>{
-    
+  
     if(play == 0){ 
-        controlIcon.innerHTML="⏸️";
-       
+        PlayStopButton.setAttribute('src','photo/pause.png');
         song.play();
         play--;
     }else if(play < 0){
-        controlIcon.innerHTML="▶️";
-      
+        PlayStopButton.setAttribute('src','photo/play.png');
+       
         song.pause();
         play = 0;
     }
@@ -28,17 +55,61 @@ controlIcon.addEventListener("click",()=>{
 });
 
 backIcon.addEventListener("click",()=>{
-    controlIcon.innerHTML="▶️";
     song.pause();
     progress.value = 0;
-    song.currentTime =progress.value;  
+    song.currentTime = progress.value;
+    songCount--; 
+    if(songCount < 0){
+        songCount = songs.length-1;
+    }
+    if(songCount > 0){
+        song.setAttribute('src', `${songs[songCount]}`);
+        song.setAttribute('type', 'video/mp4');
+
+        songPhoto.setAttribute('src', `${songsPhotos[songCount]}`);
+
+        songName.innerText = songsNames[songCount];
+        songArtist.innerText = artistNames[songCount];
+        song.play();
+        PlayStopButton.setAttribute('src','photo/pause.png');
+    }else{
+        song.setAttribute('src', `${songs[songCount]}`);
+        song.setAttribute('type', 'video/mp4');
+        songPhoto.setAttribute('src', `${songsPhotos[songCount]}`);
+        songName.innerHTML = songsNames[songCount];
+        songArtist.innerHTML = artistNames[songCount];
+        song.play();
+        PlayStopButton.setAttribute('src','photo/pause.png');
+    }
 });
 
 forwardIcon.addEventListener("click",()=>{
-        controlIcon.innerHTML="▶️";
+        
         song.pause();
-        progress.value = 400;
+        progress.value = 0;
         song.currentTime = progress.value;
+        songCount++;
+        if(songCount <= songs.length-1){
+            if(songCount > 0 ){
+                song.setAttribute('src', `${songs[songCount]}`);
+                song.setAttribute('type', 'video/mp4');
+                songPhoto.setAttribute('src', `${songsPhotos[songCount]}`);
+                songName.innerHTML = songsNames[songCount];
+                songArtist.innerHTML = artistNames[songCount];
+                song.play();
+                PlayStopButton.setAttribute('src','photo/pause.png');
+            }
+        }else {
+            songCount = 0;
+            song.setAttribute('src', `${songs[songCount]}`);
+            song.setAttribute('type', 'video/mp4');
+            songPhoto.setAttribute('src', `${songsPhotos[songCount]}`);
+            songName.innerHTML = songsNames[songCount];
+            songArtist.innerHTML = artistNames[songCount];
+            song.play();
+            PlayStopButton.setAttribute('src','photo/pause.png');
+        }
+        
 });
 
 if(play == 0 ){
@@ -49,5 +120,5 @@ if(play == 0 ){
 progress.addEventListener("change",()=>{
     song.play();
     song.currentTime = progress.value;
-    controlIcon.innerHTML="⏸️";
+    PlayStopButton.setAttribute('src','photo/pause.png');
 });
